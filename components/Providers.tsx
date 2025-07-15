84d770d28591a63dc94c2026f6022ebf78abcc21
Javed <javedkhan889971@gmail.com>
2025-07-15 12:14:48 +0500

    Vercel fix

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,14 +1,13 @@
 // components/Providers.tsx
 'use client'
 
-import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiConfig }                      from 'wagmi'
+import { WagmiConfig } from 'wagmi'
 import {
   RainbowKitProvider,
   darkTheme,
 } from '@rainbow-me/rainbowkit'
-import { wagmiConfig }   from '@/wagmi.config'
+import { wagmiConfig } from '@/wagmi.config'
 
 import '@rainbow-me/rainbowkit/styles.css'
 import '../styles/rk-overrides.css'
@@ -17,8 +16,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <WagmiConfig config={wagmiConfig}>
         <RainbowKitProvider
           chains={wagmiConfig.chains}
           modalSize="compact"
@@ -32,7 +31,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         >
           {children}
         </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiConfig>
+      </WagmiConfig>
+    </QueryClientProvider>
   )
 }
